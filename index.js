0f5962ea5e103603e09c019b9c051e794d815388
Jose Piccioni <josepiccioni@gmail.com>
2021-01-05 13:50:42 -0300

    Working example of create and delete patient

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ module.exports = class HintClient {
     this.baseUri = 'https://api.hint.com/api';
   }
 
+  // credentials() {
+  //   return({ auth: { username: this.client_key, pass: this.password } })
+  // };
+
+  async createPatient(params){
+    let credentials = { auth: { username: this.client_key, pass: this.password } }
+    let hintResponse = await axios.post(`${this.baseUri}/provider/patients`, params, credentials)
+      .then(function (response) {
+        console.log(`STATUS: ${response.status}`)
+        return({ data: response.data, status: response.status })
+      })
+      .catch(function (error) {
+        console.log(`STATUS: ${error.response.status}`)
+        return({ data: error.response.data, status: error.response.status })
+      });
+    // console.log(`data is: ${hintResponse.data}, status is: ${hintResponse.status}`);
+    return(hintResponse);
+  };
+
+  async deletePatient(params){
+    let credentials = { auth: { username: this.client_key, pass: this.password } }
+    console.log(`${this.baseUri}/provider/patients/${params.id}`,)
+    let hintResponse = await axios.delete(`${this.baseUri}/provider/patients/${params.id}`, credentials)
+      .then(function (response) {
+        console.log(`STATUS: ${response.status}`)
+        return({ data: response.data, status: response.status })
+      })
+      .catch(function (error) {
+        console.log(`STATUS: ${error.response.status}`)
+        return({ data: error.response.data, status: error.response.status })
+      });
+    // console.log(`data is: ${hintResponse.data}, status is: ${hintResponse.status}`);
+    return(hintResponse);
+  };
+
   async getLeadSources(){
     let hintResponse = await axios.get(`${this.baseUri}/provider/lead_sources`, {
       auth: {
