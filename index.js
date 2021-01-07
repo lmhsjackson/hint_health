3ac8079054c7b61924c151511d9f5d3f7297602e
Jose Piccioni <josepiccioni@gmail.com>
2021-01-07 14:46:11 -0300

    Cleaned the code to use await/async and no callbacks

--- index.js
+++ index.js
@@ -1,134 +1,111 @@
 const axios = require('axios').default;
 module.exports = class HintClient {
-  constructor(client_key, password) {
-    this.client_key = client_key;
+  constructor(clientKey, password) {
+    this.clientKey = clientKey;
     this.password = password
     this.baseUri = 'https://api.hint.com/api';
   }
 
   credentials() {
-    return({ auth: { username: this.client_key, pass: this.password } })
+    return { auth: { username: this.clientKey, pass: this.password } }
   };
 
   async deleteCard(params){
-    let hintResponse = await axios.delete(`${this.baseUri}/provider/patients/${params.patientId}/cards/${params.id}`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.delete(`${this.baseUri}/provider/patients/${params.patientId}/cards/${params.id}`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async createCard(params){
-    let hintResponse = await axios.post(`${this.baseUri}/provider/patients/${params.patient_id}/cards`, params, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.post(`${this.baseUri}/provider/patients/${params.patient_id}/cards`, params, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async deleteMembership(params){
-    let hintResponse = await axios.delete(`${this.baseUri}/provider/memberships/${params.id}`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.delete(`${this.baseUri}/provider/memberships/${params.id}`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async createMembership(params){
-    let hintResponse = await axios.post(`${this.baseUri}/provider/memberships`, params, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.post(`${this.baseUri}/provider/memberships`, params, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async createPatient(params){
-    let hintResponse = await axios.post(`${this.baseUri}/provider/patients`, params, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.post(`${this.baseUri}/provider/patients`, params, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async deletePatient(params){
-    let hintResponse = await axios.delete(`${this.baseUri}/provider/patients/${params.id}`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.delete(`${this.baseUri}/provider/patients/${params.id}`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch (error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async getLeadSources(){
-    let hintResponse = await axios.get(`${this.baseUri}/provider/lead_sources`, {
-      auth: {
-        username: this.client_key,
-        pass: this.password
-      }
-    })
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.get(`${this.baseUri}/provider/lead_sources`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async getLocations(){
-    let hintResponse = await axios.get(`${this.baseUri}/provider/locations`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.get(`${this.baseUri}/provider/locations`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 
   async getPractitioners(){
-    let hintResponse = await axios.get(`${this.baseUri}/provider/practitioners`, this.credentials())
-      .then(function (response) {
-        console.log(`STATUS: ${response.status}`)
-        return({ data: response.data, status: response.status })
-      })
-      .catch(function (error) {
-        console.log(`STATUS: ${error.response.status}`)
-        return({ data: error.response.data, status: error.response.status })
-      });
-    return(hintResponse);
+    try {
+      const response = await axios.get(`${this.baseUri}/provider/practitioners`, this.credentials())
+      console.log(`STATUS: ${response.status}`)
+      return { data: response.data, status: response.status }
+    } catch(error) {
+      console.log(`STATUS: ${error.response.status}`)
+      return { data: error.response.data, status: error.response.status }
+    };
   };
 }
